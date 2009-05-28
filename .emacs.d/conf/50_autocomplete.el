69665b646041a28fe3fc2ec3a87f502135e18c9a
Daisuke Murase <typester@cpan.org>
2009-05-28 19:23:12 +0900

    disable auto-complete-mode when input-mode is on

--- .emacs.d/conf/50_autocomplete.el
+++ .emacs.d/conf/50_autocomplete.el
@@ -3,3 +3,8 @@
 
 (define-key ac-complete-mode-map "\C-n" 'ac-next)
 (define-key ac-complete-mode-map "\C-p" 'ac-previous)
+
+;; 日本語入力中にonだとうざいので切る
+(defadvice ac-on-post-command
+  (around check-whether-input-type-is-japanese activate)
+  (or current-input-method ad-do-it))
